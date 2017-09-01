e35212d76cab6f8b4405b347ee2babe8bdd6b937
Ben Strasser <strasser@kit.edu>
2017-09-01 13:27:08 +0200

    added all.h content

---
+++ include/routingkit/all.h
@@ -0,0 +1,32 @@
+#ifndef ROUTING_KIT_ALL_H
+#define ROUTING_KIT_ALL_H
+
+// generated using ls | sed -E "s_(.*)_#include <routingkit/\1>_"
+
+#include <routingkit/bit_vector.h>
+#include <routingkit/constants.h>
+#include <routingkit/contraction_hierarchy.h>
+#include <routingkit/customizable_contraction_hierarchy.h>
+#include <routingkit/dijkstra.h>
+#include <routingkit/filter.h>
+#include <routingkit/geo_dist.h>
+#include <routingkit/geo_position_to_node.h>
+#include <routingkit/graph_util.h>
+#include <routingkit/id_mapper.h>
+#include <routingkit/id_queue.h>
+#include <routingkit/id_set_queue.h>
+#include <routingkit/inverse_vector.h>
+#include <routingkit/min_max.h>
+#include <routingkit/nested_dissection.h>
+#include <routingkit/osm_decoder.h>
+#include <routingkit/osm_graph_builder.h>
+#include <routingkit/osm_profile.h>
+#include <routingkit/osm_simple.h>
+#include <routingkit/permutation.h>
+#include <routingkit/sort.h>
+#include <routingkit/tag_map.h>
+#include <routingkit/timer.h>
+#include <routingkit/timestamp_flag.h>
+#include <routingkit/vector_io.h>
+
+#endif
